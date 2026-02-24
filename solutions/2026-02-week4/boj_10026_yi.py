86112e053617a7c75fb122023746d6a046248461
eaststar113 <igim33190@gmail.com>
2026-02-24 16:23:36 +0900

    add boj 10026

--- solutions/2026-02-week4/boj_10026_yi.py
+++ solutions/2026-02-week4/boj_10026_yi.py
@@ -1,15 +1,44 @@
-# from collections import deque
+from collections import deque
 
-# n = int(input())
-# color = [input() for _ in range(n)]
+n = int(input())
+color = [list(input()) for _ in range(n)]  ## 문자열 수정 불가능하니까 리스트로 받아야됨
+visited = [[False]*n for _ in range(n)]
 
-# def bfs(graph, start_x, start_y):
-#     visited = [[False]*n for _ in range(n)]
-#     queue = deque([start_x, start_y])
-#     visited[start_x][start_y] = True
+move_x = [-1, 1, 0, 0]
+move_y = [0, 0, -1, 1]
 
-#     while queue:
-#         vx, vy = queue.popleft()
-#         for i in graph[vx][vy]:
-#             if i == color[vx][vy] and not visited[]:
-                
+def bfs(graph, start_x, start_y):
+    queue = deque([(start_x, start_y)])
+    visited[start_x][start_y] = True
+
+    while queue:
+        vlist = queue.popleft()
+        vx, vy = vlist
+        for i in range(4):
+            nx, ny = vx + move_x[i], vy + move_y[i]
+            if 0 <= nx < n and 0 <= ny < n and graph[nx][ny] == color[start_x][start_y]:
+                if not visited[nx][ny]:
+                    visited[nx][ny] = True
+                    queue.append([nx, ny])
+
+cnt = 0      
+for i in range(n):
+    for j in range(n):
+        if not visited[i][j]:
+            bfs(color, i, j)
+            cnt += 1
+print(cnt, end=' ')
+
+for i in range(n):
+    for j in range(n):
+        if color[i][j] == 'G':
+            color[i][j] = 'R'
+
+visited = [[False]*n for _ in range(n)]
+cnt = 0      
+for i in range(n):
+    for j in range(n):
+        if not visited[i][j]:
+            bfs(color, i, j)
+            cnt += 1
+print(cnt)
